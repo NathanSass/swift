864827e0a8b9762c442a4bb4748177dccaa55e7c
E750606 <nathan.sass@chase.com>
2014-11-13 15:25:23 -0800

    have a completed todo app

--- todo.playground/section-1.swift
+++ todo.playground/section-1.swift
@@ -1,8 +1,30 @@
-// Playground - noun: a place where people can play
-
 import UIKit
 
 //Description
 //Status -> Doing, Pending, Completed (enum)
 
+enum Status{
+    case Doing, Pending, Completed
+    
+    init(){
+        self = .Pending
+    }
+}
+
+struct Task{
+    var description: String;
+    var status = Status()
+    
+    init(description: String){
+        self.description = description
+        self.status = Status.Doing
+    }
+}
+
+
+
+var todoItem = Task(description: "Learn Swift")
+
+
+todoItem.status = .Completed
 
