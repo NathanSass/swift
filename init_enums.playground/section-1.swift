356ba687481df6775d941d8fd605cccf4680fe79
E750606 <nathan.sass@chase.com>
2014-11-13 13:54:00 -0800

    example of an enum with an init

---
+++ init_enums.playground/section-1.swift
@@ -0,0 +1,22 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+
+struct Contact{
+    let firstName: String
+    let lastName: String
+    var type: String
+    
+    function fullName() -> String {
+        return firstName + 
+    }
+    
+    init(firstName: String, lastName: String){
+        self.firstName = firstName;
+        self.lastName = lastName;
+        self.type = "Friend";
+    }
+}
+
+var person = Contact(firstName: "Jon", lastName: "Smith");
