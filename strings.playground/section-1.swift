b33199847933afd9ed698eaf111df02417b965c5
E750606 <nathan.sass@chase.com>
2014-11-18 14:04:50 -0800

    about to add loops

--- strings.playground/section-1.swift
+++ strings.playground/section-1.swift
@@ -2,5 +2,71 @@
 
 import UIKit
 
-var str = "Hello, playground"
-println("List:\n 1 - Apple 2 - Bananas 6 - Oranges")
+
+println("\nList:\n1 Apple\n2 Bananas\n6 Oranges")
+println("\n\ntTab\t Tabbed")
+
+let language = "Swift"
+
+
+
+let year = 2014 //type int
+
+var version = 1.0 //type double
+
+var train: Float = 2.0
+
+var isAwesome: Bool = true
+
+
+////Unary Operators
+
+var levelscore = 0
+
+levelscore++
+
+var totalScore = 0
+totalScore = levelscore++
+totalScore
+levelscore
+
+
+
+var todo: [String] = ["buy milk", "go hard", "like real hard"]
+todo += ["planes"]
+
+println("count is", todo.count)
+
+todo.append("trains")
+
+todo[2]
+
+todo[2] = "real far"
+
+todo[2]
+
+todo.insert("boof", atIndex: 3)
+
+todo[3]
+
+///Dictionary
+
+var countries = ["CA": "Canada", "BE": "Belgium", "FR": "France"]
+
+countries["CA"]
+
+countries["US"] = "United States"
+
+countries
+let item = countries.removeValueForKey("CA")
+
+///////////////////
+///// L O O P S
+///////////////////
+
+
+
+
+
+
+
