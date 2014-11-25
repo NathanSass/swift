1498db8e706d08e1f9068f61328765a4e2092325
E750606 <nathan.sass@chase.com>
2014-11-24 18:31:04 -0800

    tuples

--- functions_optionals.playground/section-1.swift
+++ functions_optionals.playground/section-1.swift
@@ -24,3 +24,28 @@ func calcArea(#height: Int, #width: Int) -> Int{
 
 
 calcArea(height: 1000, width: 1200)
+
+//tuple
+
+func searchNames(#name: String) -> (found: Bool, description: String){
+    let names = ["Bill", "Bob", "Jane", "Tiffany", "Alex", "Elmira", "Lauren", "Kelly"]
+    var found = (false, "\(name) is not a realboy")
+    for n in names {
+        if n == name{
+            found = (true, "\(name) is a realboy")
+        }
+    }
+    return found
+}
+
+let result = searchNames(name: "John")
+
+result.0
+result.1
+
+let (found, _) = searchNames(name: "Bill")
+
+let newResult = searchNames(name: "Andrew")
+
+
+newResult.found
