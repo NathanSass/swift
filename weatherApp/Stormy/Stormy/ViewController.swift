e93ac37538a41aebda94852cbe57b084d8f853e2
Nathan Sass <Nathan.Sass@chase.com>
2015-01-13 08:43:57 -0800

    initial project files for new weather app

---
+++ weatherApp/Stormy/Stormy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stormy
+//
+//  Created by Sass, Nathan on 1/13/15.
+//  Copyright (c) 2015 Sass, Nathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
